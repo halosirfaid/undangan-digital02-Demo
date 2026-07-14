219af078ff334a2f7a8686435ac3a8b0bef3a4b9
halosirfaid <halosirfaid@gmail.com>
2026-07-14 15:30:06 +0800

    Update Data.js

--- Data.js
+++ Data.js
@@ -1,7 +1,7 @@
 // FILE DATA MEMPELAI (Klien tinggal diganti di sini)
 const dataMempelai = {
     idUndangan: "Uul-Julak", // Sesuai ID tujuan Google Sheets kamu
-    tglCountdown: "July 15, 2026 12:00:00", // Format: Bulan Tanggal, Tahun Jam:Menit:Detik
+    tglCountdown: "July 16, 2026 12:00:00", // Format: Bulan Tanggal, Tahun Jam:Menit:Detik
     
     // 1. Data Mempelai Wanita
     wanita: {
